20ea7dbf3701d75a8e687b532fc28d2149447977
Naveen Machana <naveen.machana@gmail.com>
2023-05-27 07:59:37 +0530

    practice

---
+++ arrays_hashing/Problem205_Isomorphic_Strings.py
@@ -0,0 +1,32 @@
+'''
+https://leetcode.com/problems/isomorphic-strings/description/
+Given two strings s and t, determine if they are isomorphic.
+
+Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+
+All occurrences of a character must be replaced with another character while preserving the order of characters.
+No two characters may map to the same character, but a character may map to itself.
+
+Input: s = "egg", t = "add"
+Output: true
+Example 2:
+
+Input: s = "foo", t = "bar"
+Output: false
+Example 3:
+
+Input: s = "paper", t = "title"
+Output: true
+'''
+
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        fwd, rev = {}, {}
+        for i in range(len(s)):
+            if fwd.get(s[i], t[i]) != t[i] or rev.get(t[i], s[i]) != s[i]:
+                return False
+
+            fwd[s[i]] = t[i]
+            rev[t[i]] = s[i]
+        return True
--- arrays_hashing/Problem929_Unique_Email_Address.py
+++ arrays_hashing/Problem929_Unique_Email_Address.py
@@ -36,10 +36,28 @@ from typing import List
 
 
 class Solution:
+
+    # approach - recommended
     def numUniqueEmails(self, emails: List[str]) -> int:
         unique = set()
         for e in emails:
             loc, dom = e.split('@')
             loc = loc.split('+')[0].replace('.', '')
             unique.add(loc + '@' + dom)
+        return len(unique)
+
+    # approach 2
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        unique = set()
+        for e in emails:
+            loc, i = '', 0
+            while e[i] not in ['@', '+']:
+                if e[i] != '.':
+                    loc += e[i]
+                i += 1
+
+            while e[i] != '@':
+                i += 1
+            dom = e[i + 1:]
+            unique.add((loc, dom))
         return len(unique)
