83b48b990d49397f2dd08662045dae1c5a969b07
Naveen Machana <naveen.machana@gmail.com>
2023-05-28 09:29:29 +0530

    practice

--- arrays_hashing/Problem217_Contains_Duplicate.py
+++ arrays_hashing/Problem217_Contains_Duplicate.py
@@ -1,3 +1,12 @@
+'''
+https://leetcode.com/problems/contains-duplicate/
+
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every
+element is distinct.
+
+Input: nums = [1,2,3,1]
+Output: true
+'''
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         return len(nums) != len(set(nums))
--- arrays_hashing/Problem242_Valid_Anagram.py
+++ arrays_hashing/Problem242_Valid_Anagram.py
@@ -1,3 +1,12 @@
+'''
+https://leetcode.com/problems/valid-anagram/
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+typically using all the original letters exactly once.
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+'''
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
