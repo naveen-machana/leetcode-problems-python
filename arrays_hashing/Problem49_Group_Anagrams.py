4d13ae4e27b54bff0d4be52852bdd300b5c41b84
Naveen Machana <naveen.machana@gmail.com>
2023-05-28 09:09:44 +0530

    comments

--- arrays_hashing/Problem49_Group_Anagrams.py
+++ arrays_hashing/Problem49_Group_Anagrams.py
@@ -1,3 +1,22 @@
+'''
+https://leetcode.com/problems/group-anagrams/
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+typically using all the original letters exactly once.
+
+Example 1:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+Example 2:
+
+Input: strs = [""]
+Output: [[""]]
+Example 3:
+
+Input: strs = ["a"]
+Output: [["a"]]
+'''
 import collections
 from typing import List
 
@@ -10,5 +29,4 @@ class Solution:
             for c in s:
                 count[ord(c) - ord('a')] += 1
             res[tuple(count)].append(s)
-        return res
-
+        return res.values()
