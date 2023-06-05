750912e264cdde1fefa2f50fb3c76ffb6a72bbcd
Naveen Machana <naveen.machana@gmail.com>
2023-06-05 05:40:24 +0530

    practice

--- arrays_hashing/Problem303_RangeSumQuery_Immutable.py
+++ arrays_hashing/Problem303_RangeSumQuery_Immutable.py
@@ -24,11 +24,9 @@ from typing import List
 class NumArray:
 
     def __init__(self, nums: List[int]):
-        cur = 0
-        self.psum = []
-        for n in nums:
-            cur += n
-            self.psum.append(cur)
+        self.psum = [0] * len(nums)
+        for i in range(1, len(nums)):
+            self.psum[i] = self.psum[i - 1] + nums[i]
 
     def sumRange(self, left: int, right: int) -> int:
         r = self.psum[right]
