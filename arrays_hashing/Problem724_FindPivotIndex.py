6f014d3f28dfae48913395df79fc3e33db21e7aa
Naveen Machana <naveen.machana@gmail.com>
2023-06-05 06:39:28 +0530

    practice

--- arrays_hashing/Problem724_FindPivotIndex.py
+++ arrays_hashing/Problem724_FindPivotIndex.py
@@ -21,12 +21,9 @@ from typing import List
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
         total = sum(nums)
-
-        leftSum = 0
-        for i in range(len(nums)):
-            rightSum = total - leftSum - nums[i]
-            if leftSum == rightSum:
+        left = 0
+        for i in range(nums):
+            if total - nums[i] - left == left:
                 return i
-            leftSum += nums[i]
-
+            left += nums[i]
         return -1
