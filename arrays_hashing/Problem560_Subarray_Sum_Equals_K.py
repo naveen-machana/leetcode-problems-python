eb4e82b126daab0abbc5f118a1cb875312102ecd
Naveen Machana <naveen.machana@gmail.com>
2023-06-07 07:27:50 +0530

    practice

--- arrays_hashing/Problem560_Subarray_Sum_Equals_K.py
+++ arrays_hashing/Problem560_Subarray_Sum_Equals_K.py
@@ -4,12 +4,6 @@ Given an array of integers nums and an integer k, return the total number of sub
 A subarray is a contiguous non-empty sequence of elements within an array.
 Input: nums = [1,1,1], k = 2
 Output: 2
-
-0 -> 2
-1 -> 1
-2 -> 2
-3 -> 1
-
 '''
 from typing import List
 
