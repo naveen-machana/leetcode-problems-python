ab5bbcdaa98892371139133bf7eb98fd8432bcd2
Naveen Machana <naveen.machana@gmail.com>
2023-06-12 15:45:51 +0530

    practice

--- arrays_hashing/Problem27_RemoveElement.py
+++ arrays_hashing/Problem27_RemoveElement.py
@@ -35,8 +35,8 @@ from typing import List
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         st = 0
-        for cur in range(len(nums)):
-            if nums[cur] != val:
-                nums[st] = nums[cur]
+        for v in nums:
+            if v != val:
+                nums[st] = v
                 st += 1
         return st
