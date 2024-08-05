2c0145f5a9ded6cfec0cec3d00f323309041e143
Naveen Machana <naveen.machana@gmail.com>
2024-08-05 21:38:07 +0530

    2 approaches

--- two_pointers/Problem80_RemoveDuplicatesSortedArrayII.py
+++ two_pointers/Problem80_RemoveDuplicatesSortedArrayII.py
@@ -3,7 +3,7 @@ from typing import List
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        # [0,0,1,1,1,1,2,3,3]
+        # approach 1
         p1, p2 = 0, 1
         for i in range(2, len(nums)):
             if nums[i] == nums[p1] and nums[i] == nums[p2]:
@@ -13,3 +13,19 @@ class Solution:
                 p2 += 1
                 nums[p2] = nums[i]
         return p2 + 1
+
+    # approach 2
+    # def removeDuplicates(self, nums: List[int]) -> int:
+    #     l, r = 0, 0
+    #     while r < len(nums):
+    #         count = 1
+    #         while r + 1 < len(nums) and nums[r] == nums[r + 1]:
+    #             count += 1
+    #             r += 1
+    #
+    #         for i in range(min(2, count)):
+    #             nums[l] = nums[r]
+    #             l += 1
+    #
+    #         r += 1
+    #     return l
